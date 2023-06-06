9bbe29f927acd82b66a3e40f511efa6f41e3a5ac
SilverBullet19 <a_gouasmia@estin.dz>
2023-06-06 18:24:15 +0100

    updated db configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,9 +12,9 @@ import { ConfigModule } from '@nestjs/config';
   }), AuthModule, TypeOrmModule.forRoot({
     type: "mysql",
     host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "ahmed123",
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     entities: [User],
     synchronize: true
